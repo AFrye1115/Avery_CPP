59eda384cfa08e2cca423d2528b2cedf50d25fd0
Avery Frye DS <75818897+AFrye1115@users.noreply.github.com>
2022-05-10 10:17:08 -0400

    Create avery.cpp

---
+++ avery.cpp
@@ -0,0 +1,114 @@
+#include "main.h"
+#include "subsystems.hpp"
+#include "avery.hpp"
+
+//						Avery's brainchild						//
+
+double headg;
+int multiplier = 0;
+
+//function to drive straight while holding a target heading
+void driveIt(double targHeading, double rpm){
+	  headg = gyroRotate.getHeading();
+    pros::delay(5);
+		//pros::lcd::set_text(1,std::to_string(headg));			//displays actual heading to lcd
+		gyroRotate.rotateController.setTarget(targHeading);		//sets the heading of the gyro to the desired path angle
+
+		//output to drivetrain, modeled as arcade to account for gyro correction
+	  //drive->getModel()->driveVectorVoltage(rpm, gyroRotate.rotateController.step(gyroRotate.getHeading()));
+		drive->getModel()->driveVectorVoltage(rpm, gyroRotate.rotateController.step(gyroRotate.getHeading()));
+}
+
+//more advanced driveIt that slows down as it approaches the target
+void chassisProfiling(double target, double error, long double aggr, double targHeading, int timeOut){
+  double rpm = 0;	//output speed in millivolts
+	long startTime=pros::millis();
+
+	if(target>chassis.getPosition()){
+  while(chassis.getPosition()<(target-error) && (pros::millis()<=startTime+timeOut)){						//while the current position is less than the target, drive
+	//	std::cout << std::to_string(rpm) << ", " << chassis.getPosition() << std::endl;				//output rpm to console, used for troubleshooteing
+    rpm = sqrtl(aggr*(-1*chassis.getPosition()+target));	//sqrt function to control velocity as it approaches the target position, aggr is how fast it slows down
+    if(rpm>1){
+      rpm = 1;
+    }
+    driveIt(targHeading, rpm);
+  }
+}
+else{
+	while(chassis.getPosition()>(target+error) && (pros::millis()<=startTime+timeOut)){						//while the current position is less than the target, drive
+	//	std::cout << std::to_string(rpm) << ", " << chassis.getPosition() << std::endl;				//output rpm to console, used for troubleshooteing
+    rpm = -sqrtl(-aggr*(-1*chassis.getPosition()+target));	//sqrt function to control velocity as it approaches the target position, aggr is how fast it slows down
+    if(rpm<-1){
+      rpm = -1;
+    }
+    driveIt(targHeading, rpm);
+  }
+}
+  leftDrive.moveVelocity(0);			//stops the drive after it reaches its target
+  rightDrive.moveVelocity(0);			//stops the drive after it reaches its target
+}
+
+//BEGIN Work In Progress S Curve Code
+void driveItCurve(double targHeading, double rpm, double delay, double initHead, int multiplier){
+	 headg = gyroRotate.getHeading();
+	 pros::delay(5);
+	// pros::lcd::set_text(1,std::to_string(headg));			//displays actual heading to lcd
+	 if(multiplier==1){
+	 if(chassis.getPosition()>delay){
+	 		gyroRotate.rotateController.setTarget(targHeading);		//sets the heading of the gyro to the desired path angle
+	 }
+	 else {
+		 gyroRotate.rotateController.setTarget(initHead);
+	 }
+ }
+ else{
+	 if(chassis.getPosition()<delay){
+	 		gyroRotate.rotateController.setTarget(targHeading);		//sets the heading of the gyro to the desired path angle
+	 }
+	 else {
+		 gyroRotate.rotateController.setTarget(initHead);
+	 }
+ }
+
+	 //output to drivetrain, modeled as arcade to account for gyro correction
+	 drive->getModel()->driveVectorVoltage(rpm, gyroRotate.rotateController.step(gyroRotate.getHeading()));
+}
+void curveProfiling(double target, double error, long double aggr, double targHeading, double delay, double initHead, bool forward, int timeOut){
+ double rpm = 0;																					//output speed in millivolts
+ int startTime= pros::millis();
+ chassis.tarePosition();
+ if (forward==true){
+	 multiplier = 1;
+ }
+ else{
+	 multiplier = -1;
+ }
+ if(multiplier == 1){
+ while(chassis.getPosition()<(target-error)  && (pros::millis()<=startTime+timeOut)){						//while the current position is less than the target, drive
+ //	std::cout << std::to_string(rpm) << ", " << chassis.getPosition() << std::endl;				//output rpm to console, used for troubleshooteing
+	 rpm = multiplier*sqrtl(aggr*(-1*chassis.getPosition()+target));	//sqrt function to control velocity as it approaches the target position, aggr is how fast it slows down
+	 if(rpm>1){
+		 rpm = 1;
+	 }
+	 else if(rpm<-1){
+		 rpm = -1;
+	 }
+	 driveItCurve(targHeading, rpm, delay, initHead, multiplier);
+ }
+}
+else{
+	while(chassis.getPosition()>(target+error)  && (pros::millis()<=startTime+timeOut)){
+					//while the current position is less than the target, drive
+  //	std::cout << std::to_string(rpm) << ", " << chassis.getPosition() << std::endl;				//output rpm to console, used for troubleshooteing
+ 	 rpm = multiplier*sqrtl(aggr*(-1*chassis.getPosition()-target));	//sqrt function to control velocity as it approaches the target position, aggr is how fast it slows down
+ 	 if(rpm<-1){
+ 		 rpm = -1;
+ 	 }
+	 //pros::lcd::set_text(3,std::to_string(rpm));
+ 	 driveItCurve(targHeading, rpm, delay, initHead, multiplier);
+  }
+ }
+ leftDrive.moveVelocity(0);			//stops the drive after it reaches its target
+ rightDrive.moveVelocity(0);			//stops the drive after it reaches its target
+ chassis.tarePosition();
+}
